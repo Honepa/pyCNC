db5c3bff197064ddf25cd696b847059ab2fdb269
honepa <cot1884@gmail.com>
2023-05-18 19:17:51 +0500

    bgf

--- cnc_raspi/main.py
+++ cnc_raspi/main.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     print(rpi_gpio.cnc.coordinates)
 
     img_orig = cv.imread(f'/tmp/out_{0}_{str(t)}.jpeg')
-    out = correcting_perspective(img_orig)
+    out = photo_test.find_work_area.correcting_perspective(img_orig)
     cv.imwrite('/tmp/out_linear.jpg', out)
     out_pix_coor = photo_test.find_work_area.find_board_by_cam_two('/tmp/out_linear.jpg', 240)
     coor_board_by_cam_two = photo_test.find_work_area.convert_cam_0_to_mm(out_pix_coor)
