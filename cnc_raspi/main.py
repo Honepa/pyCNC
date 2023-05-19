786b643bd83fb3d2985847737b3c7b49980b7ef5
honepa <cot1884@gmail.com>
2023-05-19 18:45:55 +0500

    main.py

--- cnc_raspi/main.py
+++ cnc_raspi/main.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
             cv.imwrite(f'/tmp/out_{2}_{t}.jpeg', img)
             img = photo_test.find_work_area.rotate(f'/tmp/out_{2}_{t}.jpeg', angle = 1.8)
             dx, dy, img = photo_test.find_work_area.find_corner_by_cam_one(img)
-            rpi_gpio.cnc.init_axis_z()
+            #rpi_gpio.cnc.init_axis_z()
             dx = int(round(dx, 2) * 100)
             dy = int(round(dy, 2) * 100)
             #print(dx, dy)
@@ -40,7 +40,7 @@ if __name__ == '__main__':
             while ((count > 150) or ((dx**2 + dy**2)**0.5 > 10)):
                 rpi_gpio.cnc.x_go(-dx, 1)
                 rpi_gpio.cnc.y_go(dy, 1)
-                img = get_frames(2)
+                img = rpi_gpio.cnc.get_frames(2)
                 t = str(int(time())%100000)
                 cv.imwrite(f'/tmp/out_{2}_{t}.jpeg', img)
                 #print(f"[INFO:] img write in /tmp/out_{2}_{t}.jpeg")
